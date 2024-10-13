68b983444c67605969da82f39555306dbb2ae3b9
Andrey <Andrey@DESKTOP-8075FT1>
2024-10-13 14:13:30 +0300

    Add project files.

---
+++ Tyuiu.KovyazinAA.Sprint2.Task0.V1.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.KovyazinAA.Sprint2.Task0.V1.Lib
+{
+    public class DataService : ISprint2Task0V1
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+            res[0] = x == y + 111;
+            res[1] = x != y + 111;
+            res[2] = x < y + 1000;
+            res[3] = x > y + 1000;
+            res[4] = x <= y + 1000;
+            res[5] = x >= y + 1000;
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.KovyazinAA.Sprint2.Task0.V1/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.KovyazinAA.Sprint2.Task0.V1.Lib;
+namespace Tyuiu.KovyazinAA.Sprint2.Task0.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Ковязин А. А. | ПКТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Ковязин Андрей Александрович | ПКТб-24-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+            Console.WriteLine("* последовательность операций не должна нарушаться) и арифметических      *");
+            Console.WriteLine("* выражений, которая вернет логическую последовательность(массив):        *");
+            Console.WriteLine("* (True, False, True, False, True, False)                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* X = 233                                                                 *");
+            int x = 233;
+            Console.WriteLine("* Y = 122                                                                 *");
+            int y = 122;
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.GetCompareOperations(x,y));
+        }
+    }
+}
